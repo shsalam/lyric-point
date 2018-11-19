00eaa6cd4186240dba136dd3c9743fea22c13346
Shifath Salam <sheef@Shifaths-MacBook-Pro.local>
2018-11-19 10:31:29 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+import {Provider} from './context';
+
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+    <Router>
+    <React.Fragment>
+          <Navbar/>
+          <div className = "container">
+          <Switch>
+            <Route exact path="/" component = {Index} />
+            </Switch>
+            </div>
+    </React.Fragment>
+    </Router>
+    </Provider>
+    );
+  }
+}
+
+export default App;
