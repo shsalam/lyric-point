a40b9fb03154542dcf0b7ed5aa23944ef9f2bfe4
Shifath Salam <sheef@Shifaths-MacBook-Pro.local>
2018-11-21 10:17:11 -0800

    created lyrics component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
-
+import Lyrics from './components/tracks/Lyrics';
 import {Provider} from './context';
 
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -18,6 +18,7 @@ class App extends Component {
           <div className = "container">
           <Switch>
             <Route exact path="/" component = {Index} />
+            <Route exact path="/lyrics/track/:id" component = {Lyrics} />
             </Switch>
             </div>
     </React.Fragment>
